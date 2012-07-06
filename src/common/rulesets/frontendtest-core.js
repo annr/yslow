ba21c3c69a8f2893362757d25b39d438bfc8a259
Ann Elizabeth Robson <aerobson@pobox.com>
2012-07-06 11:46:03 -0700

    developed first test "Serve PNGs instead of GIFs"

--- src/common/rulesets/frontendtest-core.js
+++ src/common/rulesets/frontendtest-core.js
@@ -3,21 +3,46 @@
  *
  */
 
+/**
+ * ft_pngsnotgifs:
+ *
+ * for any gif images that would be better compressed as pngs, subtract x * 3 from score of 90. (a single offender merits a "B" with this test)
+ * gifs must be a significant size (> 512 bytes) to bother about pointing them out
+ */
+
 YSLOW.registerRule({
-    id: 'ftpngsnotgifs',
-    name: 'Use PNGs instead of GIFs',
+    id: 'ft_pngsnotgifs',
+    name: 'Serve PNGs instead of GIFs',
     url: 'http://frontendtest.com/pngs-are-used-instead-of-gifs/',
     info: 'PNG image format was created to improve upon and replace GIF images; they compress better in most cases. Additionally, there are patent issues around using GIFs.',
+    category: ['images'],
 
     config: {
-         when: 'ever'
+        points: 3
     },
 
-    lint: function(doc, components, config) {
+    lint: function (doc, cset, config) {
+        var i, prop, score,
+            offenders = [],
+            comps = cset.getComponentsByType('image');
+		
+        for (i = 0; i < comps.length; i += 1) {
+            if (typeof comps[i].url !== 'undefined' && comps[i].url.match(/\.gif$/) != null) {	
+                if(comps[i].size !== 'undefined' && comps[i].size > 512 ) {
+					offenders.push(comps[i]);
+				}
+            }
+        }
+
+        score = 90 - offenders.length * parseInt(config.points, 10);
+
         return {
-            score: 100,
-            message: "There are x gifs...",
-            components: []
+            score: score,
+            message: (offenders.length > 0) ? YSLOW.util.plural(
+                '%num% GIF%s% should be converted to PNG%s% for file size savings',
+                offenders.length
+            ) : '',
+            components: offenders
         };
     }
 });
@@ -26,10 +51,9 @@ YSLOW.registerRule({
      id: 'ftcore',
      name: 'FrontendTest Core',
      rules: {
-         'ftpngsnotgifs': {}
+         'ft_pngsnotgifs': {}
      },
      weights: {
-         'ftpngsnotgifs': 3
+         'ft_pngsnotgifs': 3
      }
  });
-
